f8d1130ee1df74a30a30311895b5857b5cb081fe
osparking <jbpark03@gmail.com>
2023-04-03 21:55:05 +0900

    thymeleaf fragment layout example

---
+++ src/main/java/space/bumtiger/tleaflayout/TLeafLayoutApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.tleaflayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TLeafLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TLeafLayoutApplication.class, args);
+	}
+
+}
